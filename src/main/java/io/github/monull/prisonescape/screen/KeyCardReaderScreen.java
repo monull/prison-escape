a3fd94ad41ffc06464a580c030d9104e2fe64723
monull <monull2452@gmail.com>
2021-10-17 16:03:11 +0900

    updated

--- src/main/java/io/github/monull/prisonescape/screen/KeyCardReaderScreen.java
+++ src/main/java/io/github/monull/prisonescape/screen/KeyCardReaderScreen.java
@@ -1,11 +1,15 @@
 package io.github.monull.prisonescape.screen;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import io.github.monull.prisonescape.screen.widget.CheckBoxWidget;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
+import net.minecraft.util.Identifier;
 
 public class KeyCardReaderScreen extends Screen {
 
@@ -29,4 +33,15 @@ public class KeyCardReaderScreen extends Screen {
         textRenderer = client.textRenderer;
         setFocused(null);
     }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        this.renderBackground(matrices);
+        super.render(matrices, mouseX, mouseY, delta);
+
+        RenderSystem.setShaderTexture(0, new Identifier("prisonescape:textures/gui/bricks.png"));
+        DrawableHelper.drawTexture(matrices, 0, 0, width, height, 0, 0, 696, 564, 696, 564);
+    }
+
+
 }
