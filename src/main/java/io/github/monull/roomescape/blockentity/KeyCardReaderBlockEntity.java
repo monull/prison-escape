18a20ff42fab4afdda363ffd33d34db0a8aaf1e0
BuildTools <unconfigured@null.spigotmc.org>
2021-11-08 15:53:15 +0900

    버그는 다 수정. 디자인 업데이트 및 다른 블럭은 나중에 추가 예정

--- src/main/java/io/github/monull/roomescape/blockentity/KeyCardReaderBlockEntity.java
+++ src/main/java/io/github/monull/roomescape/blockentity/KeyCardReaderBlockEntity.java
@@ -8,6 +8,7 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.BlockPos;
 
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class KeyCardReaderBlockEntity extends BlockEntity {
 
@@ -15,10 +16,13 @@ public class KeyCardReaderBlockEntity extends BlockEntity {
 
     public KeyCardReaderBlockEntity(BlockPos pos, BlockState state) {
         super(RegistrationHandler.KEY_CARD_READER_BLOCK, pos, state);
+        for (int i = 0; i <= 4; i++) {
+            allows.add(i, false);
+        }
     }
 
     public boolean check(int level) {
-        if (allows.get(level)) return true; else return false;
+        if (allows.get(level - 1)) return true; else return false;
     }
 
     public boolean getAllows(int i) {
@@ -28,11 +32,23 @@ public class KeyCardReaderBlockEntity extends BlockEntity {
     @Override
     public NbtCompound writeNbt(NbtCompound nbt) {
         super.writeNbt(nbt);
+        nbt.putBoolean("one", allows.get(0));
+        nbt.putBoolean("two", allows.get(1));
+        nbt.putBoolean("three", allows.get(2));
+        nbt.putBoolean("four", allows.get(3));
+        nbt.putBoolean("five", allows.get(4));
         return nbt;
     }
 
     @Override
     public void readNbt(NbtCompound nbt) {
         super.readNbt(nbt);
+        if (Stream.of("one", "two", "three", "four", "five").allMatch(nbt::contains)) {
+            allows.set(0, nbt.getBoolean("one"));
+            allows.set(1, nbt.getBoolean("two"));
+            allows.set(2, nbt.getBoolean("three"));
+            allows.set(3, nbt.getBoolean("four"));
+            allows.set(4, nbt.getBoolean("five"));
+        }
     }
 }
--- src/main/java/io/github/monull/roomescape/screen/KeyCardReaderScreen.java
+++ src/main/java/io/github/monull/roomescape/screen/KeyCardReaderScreen.java
@@ -1,11 +1,12 @@
 package io.github.monull.roomescape.screen;
 
+import com.google.common.collect.ImmutableList;
 import io.github.monull.roomescape.blockentity.KeyCardReaderBlockEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.CheckboxWidget;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 
 import java.util.ArrayList;
@@ -14,7 +15,7 @@ public class KeyCardReaderScreen extends Screen {
 
     BlockEntity entity = null;
 
-    public ArrayList<Boolean> list = new ArrayList<>(5);
+    public ImmutableList<Boolean> list;
     public ArrayList<CheckboxWidget> checkboxList = new ArrayList<>(5);
 
     public KeyCardReaderScreen(BlockEntity entity) {
@@ -23,18 +24,26 @@ public class KeyCardReaderScreen extends Screen {
         super.init(MinecraftClient.getInstance(), MinecraftClient.getInstance().getWindow().getWidth(), MinecraftClient.getInstance().getWindow().getHeight());
         this.entity = entity;
         if (entity instanceof KeyCardReaderBlockEntity) {
-            for (int i = 0; i <= 4; i++) {
-                boolean bool = ((KeyCardReaderBlockEntity) entity).getAllows(i);
-                list.add(bool);
-                System.out.println(bool);
-            }
+            list = ImmutableList.copyOf(((KeyCardReaderBlockEntity) entity).allows);
         }
-
+        for (int i = 0; i <= 4; i++) {
+            CheckboxWidget box = new CheckboxWidget(30, 30 + 20 * (i - 1), 20, 20, new LiteralText(String.valueOf(i + 1)), list.get(i));
+            checkboxList.add(box);
+            addDrawableChild(box);
+        }
+        addDrawableChild(new ButtonWidget(50, 50, 50, 20, new LiteralText("exit"), button -> {
+            this.onClose();
+        }));
     }
 
     @Override
-    public void tick() {
-
+    public void onClose() {
+        super.onClose();
+        for (int i = 0; i <= 4; i++) {
+            if (entity instanceof KeyCardReaderBlockEntity) {
+                ((KeyCardReaderBlockEntity) entity).allows.set(i, checkboxList.get(i).isChecked());
+            }
+        }
     }
 
     @Override
